06ff5263d8dab759d13510db59fa9ade343e4a63
smacoh <166351536+smacoh@users.noreply.github.com>
2025-01-31 21:45:16 -0800

    reupload to site

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');console.log(projectsContainer);
 renderProjects(latestProjects, projectsContainer, 'h2');
 
+
 // Lab 4: Step 3.2: Fetching the data with Javascript
 const githubData = await fetchGitHubData('smacoh');
 const profileStats = document.querySelector('#profile-stats');
 if (profileStats) {
     profileStats.innerHTML = `
-    
+
           <dl>
             <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
             <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
